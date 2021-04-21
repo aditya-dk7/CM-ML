bb9b0287a0f208226f9c5869b90022c406b28e1c
Raunak65 <RaunakChandak65@gmail.com>
2021-04-21 22:15:12 +0530

    optimization and temperature field included

--- firebaseAuth/upload.py
+++ firebaseAuth/upload.py
@@ -1,20 +1,24 @@
-import pyrebase, sys
-from datetime import datetime 
+import pyrebase
+import sys
+from datetime import datetime
 from APIkeyAndConfig import firebaseConfig, email, password
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 auth = firebase.auth()
 storage = firebase.storage()
-user = auth.sign_in_with_email_and_password(email,password)
+user = auth.sign_in_with_email_and_password(email, password)
 userIDGet = str(user['localId'])
 dateTimeCurrent = str(datetime.now())
 imageFileName = sys.argv[1]
-results_for_storage = storage.child(userIDGet).child(dateTimeCurrent).put(imageFileName, user['idToken'])
-#print(results_for_storage)
+person_temperature = str(sys.argv[2])
+results_for_storage = storage.child(userIDGet).child(
+    dateTimeCurrent).put(imageFileName, user['idToken'])
+# print(results_for_storage)
 md5hashForImage = str(results_for_storage['md5Hash'])
 nameImage = str(results_for_storage['name'])
 data = {
     "nameImage": nameImage,
-    "md5Hash": md5hashForImage
+    "md5Hash": md5hashForImage,
+    "userTemperature": person_temperature
 }
 results = db.child("users").child(userIDGet).push(data, user["idToken"])
